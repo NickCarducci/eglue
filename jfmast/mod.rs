5682f67acc456c085d0eb292dda3ad370e876465
Nick Carducci <nmcarducci@gmail.com>
2022-07-11 20:06:44 -0400

    Update mod.rs

--- jfmast/mod.rs
+++ jfmast/mod.rs
@@ -1,3 +1,6 @@
+//bipartisan dipoles against marxism
+//inelastic over demand of preferences
+
 use futures::executor::LocalPool;
 
 pub fn pathify(path: &str) -> std::path::PathBuf {
@@ -7,20 +10,19 @@ pub fn pathify(path: &str) -> std::path::PathBuf {
     return input_file;
 }
 
-struct DropOnce<'a> {
-    _bosun: dyn Mak<&'a mut std::sync::Once<'a>>,
+struct DropOnce{
+    _bosun: dyn Mak<std::sync::Once, Output = cc::Build>, //::new()
 }
-trait Mak {//fn resolve(self) -> Self::Output;
-    type Output;//static types before compilation! (concrete type)
+trait Mak<'a,T:'a> {//<'a,T:'a> 
+    fn resolve(self) -> cc::Build;
+    type Output; //static types before compilation! (concrete type)
     fn _bosun() {
         let lock: std::path::PathBuf = pathify("./exec.c"); //.iter();
         cc::Build::new().file(lock).expand(); //= Default::default().await
     }
-    fn new(){
-        
-    }
+    fn new() {}
 }
-impl<'a> std::future::Future for DropOnce<'a> {
+impl<'a> std::future::Future for DropOnce{
     fn poll(&'a mut self) -> &'a mut Self {
         println!("poll");
         self
@@ -28,5 +30,34 @@ impl<'a> std::future::Future for DropOnce<'a> {
 }
 fn main() {
     let mut pool = LocalPool::new();
-     pool.run_until(DropOnce._bosun);
+    let _bosun = DropOnce;
+    pool.run_until(_bosun);
 }
+
+//hey you guys, decent.
+//why was george floyd's any less stealing than lending
+//i'll tell you why
+//_ _ plant
+//competing with consumers for exclusion is criminal
+//ascertainably exclusively. excess
+//"[suuuck it]." no! "Is oral sex canonical?"
+//it's my studio
+//one of these days I'm going to make a faggot out of you, proving you no more than a squire
+//why was George Floyd's currencty any less stealing than lending?
+//I'll tell you why
+//please please please please please please use United States
+//because that is about domestic violence republic
+
+//take it up with stacked leisure exchanges in payment installments
+//inelasticity over demand
+
+//no one wants vertical horizon tax free ; it's not a tax break it is a tax favor
+//gift damage by extra installment in product
+
+//improve your skills bro and abstract
+//dipoles bipartisan covenance against non voters
+
+//'don't say my name.' G-d is mature now
+
+//decent lie stupid law only
+//rollover padrone abortion doctors
