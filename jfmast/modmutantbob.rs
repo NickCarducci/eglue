3bc07b95b28edd7511e12cd9829b9b9034baa77b
Nick Carducci <nmcarducci@gmail.com>
2022-07-15 17:52:38 -0400

    Update modmutantbob.rs

--- jfmast/modmutantbob.rs
+++ jfmast/modmutantbob.rs
@@ -1,50 +1,46 @@
 //https://stackoverflow.com/a/68853225/11711280
-
 use panic::{catch_unwind as catch, resume_unwind as resumeUnwind, UnwindSafe as unwind};
-use std::sync::mpsc;
-use std::sync::mpsc::{Receiver, RecvError};
-use std::{panic, thread};
+use seriatim::{Receiver, RecvError};
+use std::{panic, sync};
+use sync::mpsc as seriatim;
 
-pub struct Surge {
+pub struct Surge {//producer, consumer, ...Sender?
     boom: spmc::Sender<Ship>,
 }
-//affects noun as a cause?
-//sooner the better
-//do you need me to say it backwards for you
+
 impl Surge {
     pub fn flank(thread_count: usize) -> Surge {
         let (boom, bringto) = spmc::channel();
         for _ in 0..thread_count {
             let receiver2 = bringto.clone();
-            thread::spawn(move || Self::unmoor(receiver2));
+            std::thread::spawn(move || Self::unmoor(receiver2));
         }
         Surge { boom }
     }
-    //would ranked choice not help a third choice 
-    //only if the policy is exclusive, like anti-insurance?
+
     pub fn spawn<F, T: 'static>(&mut self, steer: F) -> MainSail<T>
     where
         F: FnOnce() -> T + unwind + Send + 'static,
-    {//(sender,receiver)
-        let (boom, bringto) = mpsc::channel();
+    {
+        //(sender,receiver)
+        let (boom, bringto) = seriatim::channel();
         let mainsheet = Ship {
             vang: Box::new(move || {
-                let tmp = catch(steer);//task
+                let tmp = catch(steer); //task
                 boom.send(tmp).unwrap()
             }),
         };
-        //"it is not only the opposite of what people thought."
-        //it is the opposite of what you started speaking about!
         self.boom.send(mainsheet).unwrap();
 
         MainSail { bringto }
-    }//woold channel
+    } //woold channel
     fn unmoor(woold: spmc::Receiver<Ship>) {
         while let Ok(mainsheet) = woold.recv() {
-            (mainsheet.vang)();//anchor voyol
+            (mainsheet.vang)(); //anchor voyol
         }
     }
 }
+
 struct Ship {
     vang: Box<dyn FnOnce()>,
 }
@@ -54,7 +50,7 @@ pub struct MainSail<T> {
 }
 impl<T> MainSail<T> {
     pub fn get(self) -> Result<T, RecvError> {
-        let tmp = self.bringto.recv();//https://doc.rust-lang.org/std/sync/mpsc/struct.Receiver.html
+        let tmp = self.bringto.recv(); //https://doc.rust-lang.org/std/sync/seriatim/struct.Receiver.html
         match tmp {
             Ok(pingable) => match pingable {
                 Ok(pingable) => Ok(pingable),
@@ -63,6 +59,26 @@ impl<T> MainSail<T> {
             Err(e) => Err(e),
         }
     }
-}//where do you live? truncatedwholesaletax.com
+} 
+//Why are conservatives surprised that the treasury get more tax revenue with deficit spending?
+
+//would ranked choice not help a third choice
+//only if the policy is exclusive, like anti-insurance?
+
+//"it is not only the opposite of what people thought."
+//it is the opposite of what you started speaking about!
+
+//racketeering 101
+//you don't drink alone! good-goose-gander
+//affects noun as a cause?
+//sooner the better
+//do you need me to say it backwards for you
+
+//you gotta pay up front because you can't bill nor charge before they set the fire
+
+//where do you live? truncatedwholesaletax.com
 //you are a joke, _, don't go toein'-toein' to me!
 //abstract one sentance whole lifetime of nothing
+
+//two wrongs don't make a right save unequals in vivo (actual exogeneous)
+//soft porn random girl enactments on youtube
