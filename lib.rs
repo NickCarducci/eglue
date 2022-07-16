dd79dc3d8ff513237367a8a4d6a8d2d9d1f558b2
Nick Carducci <nmcarducci@gmail.com>
2022-07-16 03:54:45 -0400

    Update lib.rs

--- lib.rs
+++ lib.rs
@@ -12,7 +12,7 @@ use std::future::IntoFuture;
 use wasm_bindgen::JsValue;
 use wasm_bindgen_futures::future_to_promise;
 
-#[wasm_bindgen] 
+#[wasm_bindgen]
 pub async fn main() -> Result<Promise, JsValue> {
     struct Ship;
     impl IntoFuture for Ship {
@@ -21,11 +21,10 @@ pub async fn main() -> Result<Promise, JsValue> {
         fn into_future(self) -> Self::IntoFuture {
             let lock: std::path::PathBuf = pathify("./exec.c");
             let app: vec![u8] = cc::Build::new().file(lock).expand();
+            println!("{}", app);
         }
     }
-    let result: Promise = future_to_promise(
-        Ship.await,
-    );
+    let result: Promise = future_to_promise(Ship.await);
     Ok(result)
 }
 //https://github.com/rust-lang/rust/issues/67644
