aede614c79ec79cc3c141365f00658ec6a583764
Nick Carducci <nmcarducci@gmail.com>
2022-07-24 12:58:38 -0400

    Update lib.rs

--- lib.rs
+++ lib.rs
@@ -1,54 +1,195 @@
 //https://github.com/cloudflare/workers-rs/issues/94
 //"you'll need to define the Durable Object in a separate module. ...this:"
 //"https://github.com/cloudflare/workers-rs/blob/main/worker-sandbox/src/lib.rs"
+use worker::{
+    /*console_log, Headers,RequestInit, Fetch,*/ event, Env, Request, Response, Result, Router,
+};
 
-//use wasm_bindgen::JsValue;
-use std::sync::atomic::{AtomicBool, Ordering};
-//use web_sys::Url; //web_sys
-use worker::{/*Headers,RequestInit,*/event, Env, Request, Response, Result, Router};
-
-mod utils;
 mod index;
+/*mod utils;
 static GLOBAL_STATE: AtomicBool = AtomicBool::new(false);
 #[event(start)]
 pub fn start() {
     utils::set_panic_hook();
     // Change some global state so we know that we ran our setup function.
     GLOBAL_STATE.store(true, Ordering::SeqCst);
-}
+}*/
+
+/*#[wasm_bindgen]
+pub fn handle(option:Option<String>) ->Resultt<webRes,worker::Error>  {
+    //let req: Request = req.dyn_into()?;
+    //let mut init = ResponseInit::new();
+    //init.status(200);
+    let option = option.as_deref();
+    return webRes::new_with_opt_str(option);//webRes::new_with_opt_str(None, &init);
+}*/
 
 struct SomeSharedData {
-    data: u8, //regex::Regex,
+    //data: u8, //regex::Regex,
 }
 //https://github.com/rust-lang/rfcs/pull/2600; //https://github.com/rust-lang/rust/issues/23416, type ascription ob.key: Type=value
-#[event(fetch)] //#![feature(type_ascription)]//https://stackoverflow.com/questions/36389974/what-is-type-ascription
+#[event(fetch,respond_with_errors)] //#![feature(type_ascription)]//https://stackoverflow.com/questions/36389974/what-is-type-ascription
 pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Response> {
+    fn origin_url(req_headers: &worker::Headers) -> std::string::String {
+        return match req_headers.get("Origin").unwrap() {
+            Some(value) => value,
+            None => "".to_owned() + "", //Response::empty(),
+        };
+    }
     let info = SomeSharedData {
-        data: 0, //regex::Regex::new(r"^\d{4}-\d{2}-\d{2}$").unwrap(),
+        //data: 0, //regex::Regex::new(r"^\d{4}-\d{2}-\d{2}$").unwrap(),
     };
+    /*match
+    fn options(req_headers: &worker::Headers, cors_origin: &str) {
+        //
+    }*//* {
+          Some(re)=> Response::ok(&(re)),
+          None => Response::ok(&("options fault ".to_owned()+""))
+      }*/
+    /*return*/
+    /*fn getIndexResponse() -> Result<Response> {
+        let mut res_headers = worker::Headers::new();
+            //headers.set("x-foo", "waffles")?;
+        return Ok(Response::ok("waffles")?.with_headers(res_headers));
+    }*/
+    let _result = Router::with_data(info) // if no data is needed, pass `()` or any other valid data
+        /*if (request.method === "OPTIONS")
+          return new Response(`preflight response for POST`, {
+            status: 200,
+            message: `preflight response for POST`,
+            headers: {
+              "Access-Control-Allow-Headers": [
+                //"Access-Control-Allow-Origin",
+                "Access-Control-Allow-Methods",
+                "Content-Type"
+                //"Origin",
+                //"X-Requested-With",
+                //"Accept"
+              ],
+              "Access-Control-Allow-Methods": ["POST", "OPTIONS"]
+            }
+          });
+        return await noException(request, env);*/
+          /*.options("/ *catchall", |_, ctx| {
+              Response::ok(ctx.param("catchall").unwrap())
+          })*/
+        .options("/*catchall", |_, ctx| {
+            Response::ok(ctx.param("catchall").unwrap())
+        })
+        .options("/:id", |_, _| {
+            return Response::error(&("option (where?) ".to_owned() + ""), 404);
+        })
+        .options_async("/", |req, ctx| async move {
+            let req_headers = req.headers(); //<&worker::Headers>
+            let cors_origin = &ctx.var("CORS_ORIGIN")?.to_string(); //<&str>
 
-    let router = Router::with_data(info); // if no data is needed, pass `()` or any other valid data
-
-    router
-        .get_async("/:id", |_req, ctx| async move {
-            //get, async move
-            let namespace = ctx.durable_object("EXAMPLE_CLASS_DURABLE_OBJECT")?;
-            let stub = namespace.id_from_name("DurableObjectExample")?.get_stub()?;
-            return match _req.url().ok() {
-                //Result.ok to Option
-                Some(url) => match url.host_str() {
-                    //Option
-                    Some(url) => match [ 
-                            "https://sausage.vau.money","https://vau.money","https://jwi5k.csb.app","https://i7l8qe.csb.app"//,"https://mastercard-backbank.backbank.workers.dev"
-                        ].iter().any(|&s| s == url) {
-                            true => stub.fetch_with_str(url).await,
-                            false => Response::ok(&("no access from ".to_owned()+url))//&format!("no access from ")
-                        },
-                    None => Response::ok(&("cannot host_str() ".to_owned()+""))//eprintln!("noope"),
-                },
-                None => Response::ok(&("cannot req.url() ".to_owned()+"")),
+            return match [
+                "https://sausage.vau.money",
+                "https://vau.money",
+                "https://jwi5k.csb.app",
+                "https://i7l8qe.csb.app", //,"https://mastercard-backbank.backbank.workers.dev"
+            ]
+            .iter()
+            .any(|&s| s == cors_origin)
+            {
+                true => {
+                    //options(req_headers, cors_origin)
+                    //https://rodneylab.com/using-rust-cloudflare-workers/
+                    //fn preflight_response(_,_)->Result<Response> {
+                    let origin = origin_url(req_headers);
+                    let mut res_headers = worker::Headers::new();
+                    res_headers.set("Access-Control-Allow-Headers", "Content-Type")?;
+                    res_headers.set("Access-Control-Allow-Methods", "POST")?;
+                    //res_headers.set("Vary", "Origin")?;
+                    for origin_element in cors_origin.split(',') {
+                        if origin.eq(origin_element) {
+                            res_headers.set("Access-Control-Allow-Origin", &origin)?;
+                            break;
+                        };
+                    }
+                    res_headers.set("Access-Control-Max-Age", "86400")?;
+                    let req_method = req.method();
+                    return Response::ok(req_method).map(|resp| resp.with_headers(res_headers));
+                    /*.unwrap()
+                    .with_headers(res_headers)
+                    .with_status(204)*/
+                }
+                false => Response::error(&("no access from ".to_owned() + cors_origin), 403), //&format!("no access from ")
             };
-        })//https://github.com/NickCarducci/mastercard-backbank/blob/main/lib.rs
+        })
+        .get("/:id", |_, _| {
+            return Response::error(&("get (where?) ".to_owned() + ""), 404);
+            //return Ok(Response::error(&("get (where?) ".to_owned() + ""), 404)?);
+        })
+        .get("/", |_, _| {
+            return Response::error(&("get (method?) ".to_owned() + ""), 405);
+        })
+        .post_async("/", |_req, ctx| async move {
+            //https://stackoverflow.com/questions/70309403/updating-html-canvas-imagedata-using-rust-webassembly
+            // when calling fetch to a Durable Object, a full URL must be used. Alternatively, a
+            // compatibility flag can be provided in wrangler.toml to opt-in to older behavior:
+            // https://developers.cloudflare.com/workers/platform/compatibility-dates#durable-object-stubfetch-requires-a-full-url
+            let url: worker::Url = _req.url()?;
+            return Response::ok(match url.host_str() {
+                None => "cannot host_str() ".to_owned() + "",
+                //Option resolution =>
+                Some(url) => {
+                    //get, async move
+                    let binding = ctx.durable_object("EXAMPLE_CLASS_DURABLE_OBJECT");
+                    return match binding.is_err() {
+                        false => Response::error("none", 405),
+                        true => {
+                            let namespace = binding?;
+                            let _stub =
+                                namespace.id_from_name("DurableObjectExample")?.get_stub()?;
+
+                                /*let mut opts = RequestInit::new();
+                                opts.method("GET");
+                                opts.mode(RequestMode::Cors);
+                                let url =
+                                    format!("https://api.github.com/repos/{}/branches/master", repo);
+                                let request = Request::new_with_str_and_init(&url, &opts)?;
+
+                            request
+                                .headers()
+                                .set("Accept", "application/vnd.github.v3+json")?;*/
+                            return Response::ok(url);
+                            //return stub.fetch_with_str(&url).await;
+                        }
+                    };
+                }
+            });
+        })
+        .or_else_any_method("/*catchall", |req, ctx| {
+            /*console_log!(
+                "[or_else_any_method_async] caught: {}",
+                ctx.param("catchall").unwrap_or(&"?".to_string())
+            );*/
+            /*Fetch::Url("https://github.com/404".parse().unwrap())
+            .send()
+            .await
+            .map(|resp| resp.with_status(404)) */
+
+            let req_headers = req.headers();
+            let cors_origin = &ctx.var("CORS_ORIGIN")?.to_string(); //<&str>
+
+            let mut res_headers = worker::Headers::new();
+            res_headers.set("Access-Control-Allow-Headers", "Content-Type")?;
+            res_headers.set("Access-Control-Allow-Methods", "POST")?;
+            //headers.set("Vary", "Origin")?;
+            let origin = origin_url(req_headers);
+            for origin_element in cors_origin.split(',') {
+                if origin.eq(origin_element) {
+                    res_headers.set("Access-Control-Allow-Origin", &origin)?;
+                    break;
+                };
+            }
+            //return Response::ok("waffles")?.with_headers(res_headers);
+            return Response::ok("waffles").map(|resp| resp.with_headers(res_headers));
+        })
         .run(req, env)
-        .await
+        .await; //;
+    //return result;
+    return Response::ok("");
+    //return Response::error("key missing", 400);
 }
