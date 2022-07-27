497cc7d762a91ab22b034789a0bada1730948ef1
Nick Carducci <nmcarducci@gmail.com>
2022-07-27 18:56:35 -0400

    Update lib.rs

--- lib.rs
+++ lib.rs
@@ -2,7 +2,15 @@
 //"you'll need to define the Durable Object in a separate module. ...this:"
 //"https://github.com/cloudflare/workers-rs/blob/main/worker-sandbox/src/lib.rs"
 
+// We're able to specify a start event that is called when the WASM is initialized before any
+// requests. This is useful if you have some global state or setup code, like a logger. This is
+// only called once for the entire lifetime of the worker.
+//use wasm_bindgen::JsValue;
 use std::sync::atomic::{AtomicBool, Ordering /*,Result as Resultt*/};
+//use web_sys::Url; //web_sys
+//use wasm_bindgen::prelude::wasm_bindgen;
+//use wasm_bindgen_futures::ResponseInit; wrong
+//use web_sys::{ResponseInit,Response as webRes};
 
 use worker::{
     /*console_log, Headers,RequestInit, Fetch,*/ event, Env, Request, Response, Result, Router,
@@ -18,12 +26,22 @@ pub fn start() {
     GLOBAL_STATE.store(true, Ordering::SeqCst);
 }
 
+use serde::Serialize;
+#[derive(Serialize)]
+struct Product {
+    url: String,
+}
+#[derive(Serialize)]
+struct Error {
+    err: String,
+}
 struct SomeSharedData {
     //data: u8, //regex::Regex,
 }
 //https://github.com/rust-lang/rfcs/pull/2600; //https://github.com/rust-lang/rust/issues/23416, type ascription ob.key: Type=value
-#[event(fetch,respond_with_errors)] //#![feature(type_ascription)]//https://stackoverflow.com/questions/36389974/what-is-type-ascription
+#[event(fetch, respond_with_errors)] //#![feature(type_ascription)]//https://stackoverflow.com/questions/36389974/what-is-type-ascription
 pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Response> {
+    //use the request parameter of the router instead else return closure/(pointer)
     fn origin_url(req_headers: &worker::Headers) -> std::string::String {
         return match req_headers.get("Origin").unwrap() {
             Some(value) => value,
@@ -33,38 +51,35 @@ pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Respo
     let info = SomeSharedData {
         //data: 0, //regex::Regex::new(r"^\d{4}-\d{2}-\d{2}$").unwrap(),
     };
-
-    let _result = Router::with_data(info) // if no data is needed, pass `()` or any other valid data
-        /*if (request.method === "OPTIONS")
-          return new Response(`preflight response for POST`, {
-            status: 200,
-            message: `preflight response for POST`,
-            headers: {
-              "Access-Control-Allow-Headers": [
-                //"Access-Control-Allow-Origin",
-                "Access-Control-Allow-Methods",
-                "Content-Type"
-                //"Origin",
-                //"X-Requested-With",
-                //"Accept"
-              ],
-              "Access-Control-Allow-Methods": ["POST", "OPTIONS"]
-            }
-          });
-        return await noException(request, env);*/
-          /*.options("/ *catchall", |_, ctx| {
-              Response::ok(ctx.param("catchall").unwrap())
-          })*/
-        .options("/*catchall", |_, ctx| {
-            Response::ok(ctx.param("catchall").unwrap())
+    let router = Router::with_data(info); // if no data is needed, pass `()` or any other valid data
+                                          /*if (request.method === "OPTIONS")
+                                            return new Response(`preflight response for POST`, {
+                                              status: 200,
+                                              message: `preflight response for POST`,
+                                              headers: {
+                                                "Access-Control-Allow-Headers": [
+                                                  //"Access-Control-Allow-Origin",
+                                                  "Access-Control-Allow-Methods",
+                                                  "Content-Type"
+                                                  //"Origin",
+                                                  //"X-Requested-With",
+                                                  //"Accept"
+                                                ],
+                                                "Access-Control-Allow-Methods": ["POST", "OPTIONS"]
+                                              }
+                                            });
+                                          return await noException(request, env);*/
+    /*.options("/ *catchall", |_, ctx| {
+        Response::ok(ctx.param("catchall").unwrap())
+    })*/
+    router
+        .get("/", |_, _| {
+            Response::error(&("{error:get (method?) ".to_owned() + "}" + ""), 405)
         })
-        .options("/:id", |_, _| {
-            return Response::error(&("option (where?) ".to_owned() + ""), 404);
-        })
-        .options_async("/", |req, ctx| async move {
+        .options("/", |req, _ctx| {
             let req_headers = req.headers(); //<&worker::Headers>
-            let cors_origin = &ctx.var("CORS_ORIGIN")?.to_string(); //<&str>
-
+            let cors_origin = origin_url(req_headers);
+            //let cors_origin = &ctx.var("CORS_ORIGIN")?.to_string(); //<&str>
             return match [
                 "https://sausage.vau.money",
                 "https://vau.money",
@@ -78,96 +93,71 @@ pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Respo
                     //options(req_headers, cors_origin)
                     //https://rodneylab.com/using-rust-cloudflare-workers/
                     //fn preflight_response(_,_)->Result<Response> {
-                    let origin = origin_url(req_headers);
                     let mut res_headers = worker::Headers::new();
+                    res_headers.set("Access-Control-Allow-Origin", "*")?;
                     res_headers.set("Access-Control-Allow-Headers", "Content-Type")?;
                     res_headers.set("Access-Control-Allow-Methods", "POST")?;
                     //res_headers.set("Vary", "Origin")?;
                     for origin_element in cors_origin.split(',') {
-                        if origin.eq(origin_element) {
-                            res_headers.set("Access-Control-Allow-Origin", &origin)?;
+                        if cors_origin.eq(origin_element) {
+                            res_headers.set("Access-Control-Allow-Origin", &cors_origin)?;
                             break;
                         };
                     }
                     res_headers.set("Access-Control-Max-Age", "86400")?;
                     let req_method = req.method();
-                    return Response::ok(req_method).map(|resp| resp.with_headers(res_headers));
-                    /*.unwrap()
-                    .with_headers(res_headers)
-                    .with_status(204)*/
+                    Response::ok(req_method).map(|resp| resp.with_headers(res_headers))
                 }
-                false => Response::error(&("no access from ".to_owned() + cors_origin), 403), //&format!("no access from ")
+                false => Response::error(&("no access from ".to_owned() + &cors_origin), 403), //&format!("no access from ")
             };
-        })
-        .get("/:id", |_, _| {
-            return Response::error(&("get (where?) ".to_owned() + ""), 404);
-            //return Ok(Response::error(&("get (where?) ".to_owned() + ""), 404)?);
-        })
-        .get("/", |_, _| {
-            return Response::error(&("get (method?) ".to_owned() + ""), 405);
-        })
+        }) //https://community.cloudflare.com/t/fetch-post-type-error-failed-to-execute-function/311016/3?u=carducci
         .post_async("/", |_req, ctx| async move {
-            let url: worker::Url = _req.url()?;
-            return Response::ok(match url.host_str() {
-                None => "cannot host_str() ".to_owned() + "",
-                //Option resolution =>
-                Some(url) => {
-                    //get, async move
-                    let binding = ctx.durable_object("EXAMPLE_CLASS_DURABLE_OBJECT");
-                    return match binding.is_err() {
-                        false => Response::error("none", 405),
-                        true => {
-                            let namespace = binding?;
-                            let _stub =
-                                namespace.id_from_name("DurableObjectExample")?.get_stub()?;
+            //async may ask for pointer/(closure) with (1) both _[vars] like _req,_ctx, as well as (2) expression `return;`
+            /*UX for Post requests should enable textual resolutions
+             I would like to suggest a UX solution that would be least astonishing, and bring this into the workflow of your products in other facets,
+             or explain why errors
 
-                            /*let mut opts = RequestInit::new();
-                            opts.method("GET");
-                            opts.mode(RequestMode::Cors);
-                            let url =
-                                format!("https://api.github.com/repos/{}/branches/master", repo);
-                            let request = Request::new_with_str_and_init(&url, &opts)?;
+            https://community.cloudflare.com/t/fetch-post-type-error-failed-to-execute-function/311016/3?u=carducci
+             */
+            //let url = Url::new(&_req.url()?)?;
+            //let url =  req.url()?;
+            //let mut res_headers = worker::Headers::new();
+            //return Response::ok(url.host_str())//.map(|resp| resp.with_headers(res_headers));;
 
-                            request
-                                .headers()
-                                .set("Accept", "application/vnd.github.v3+json")?;*/
-                            return Response::ok(url);
-                            //return stub.fetch_with_str(&url).await;
-                        }
-                    };
+            /*return Response::from_json(&Product {
+                url: "stub.fetch_with_str(https://mastercard-backbank.backbank.workers.dev/).await"
+                    .to_string(),
+            });*/
+            /*return match req.url()?.host_str() {
+            None => Response::from_json(&Error {
+                err: "cannot _req.url()?.host_str()".to_string(),
+            }), //,505
+            //Option(resolution) => {explicit return; resolves in closure}
+            Some(url) => {*/
+            //Response::from_json(&Product{url: url.to_string()}) //.map(|resp| resp.with_headers(res_headers));;
+            //get, async move
+            let binding = ctx.durable_object("EXAMPLE_CLASS_DURABLE_OBJECT");
+            return match binding.is_err() {
+                true => Response::error("EXAMPLE_CLASS_DURABLE_OBJECT is_err", 405),
+                false => {
+                    let namespace = binding?;
+                    let stub = namespace.id_from_name("DurableObjectExample")?.get_stub()?;
+                    stub.fetch_with_str("https://mastercard-backbank.backbank.workers.dev/")
+                        .await //this is not like fetching the resource again, just the stub
+                    /*A full URL must be used (when calling fetch on a Durable Object).
+                    Also, a wrangler.toml compatibility flag can opt-in to[ the otherwise] 
+                    [older behavior](https://developers.cloudflare.com/workers/platform/compatibility-dates#durable-object-stubfetch-requires-a-full-url).
+
+                    Astonishingly, I would have figured out to ask this sooner if Post requests could enable 
+                    [textual resolutions](https://community.cloudflare.com/t/fetch-post-type-error-failed-to-execute-function/311016/3?u=carducci).
+                    */
                 }
-            });
-        })
-        .or_else_any_method("/*catchall", |req, ctx| {
-            /*console_log!(
-                "[or_else_any_method_async] caught: {}",
-                ctx.param("catchall").unwrap_or(&"?".to_string())
-            );*/
-            /*Fetch::Url("https://github.com/404".parse().unwrap())
-            .send()
-            .await
-            .map(|resp| resp.with_status(404)) */
-
-            let req_headers = req.headers();
-            let cors_origin = &ctx.var("CORS_ORIGIN")?.to_string(); //<&str>
-
-            let mut res_headers = worker::Headers::new();
-            res_headers.set("Access-Control-Allow-Headers", "Content-Type")?;
-            res_headers.set("Access-Control-Allow-Methods", "POST")?;
-            //headers.set("Vary", "Origin")?;
-            let origin = origin_url(req_headers);
-            for origin_element in cors_origin.split(',') {
-                if origin.eq(origin_element) {
-                    res_headers.set("Access-Control-Allow-Origin", &origin)?;
-                    break;
-                };
-            }
-            //return Response::ok("waffles")?.with_headers(res_headers);
-            return Response::ok("waffles").map(|resp| resp.with_headers(res_headers));
+            };
+            //}}
         })
         .run(req, env)
-        .await; //;
-    //return result;
-    return Response::ok("");
-    //return Response::error("key missing", 400);
+        .await// == Ok for Result<T> not return (hoist);
+        //https://stackoverflow.com/questions/60020738/expected-enum-stdresultresult-found
+              
+}
 }
